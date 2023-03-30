2270edd976bdaa056357007020bb5150180de379
SawaniX <tomekwalburg@wp.pl>
2023-03-30 23:09:28 +0200

    added splitting to segments

---
+++ read_file.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
+
+class Data:
+    def __init__(self):
+        dir_path = os.path.dirname(__file__)
+        samplerate, data = wavfile.read(dir_path + '/wiedzmin_1_channel.wav')
+        self.samplerate = samplerate
+        self.data = data
+
+    def plot(self):
+        time = np.linspace(0., self.data.shape[0] / self.samplerate, self.data.shape[0])
+        plt.plot(time, self.data, label="Left channel")
+        plt.legend()
+        plt.xlabel("Time [s]")
+        plt.ylabel("Amplitude")
+        plt.show()
+        
--- receiver.py
+++ receiver.py
@@ -1,21 +1,6 @@
 class Receiver:
-    def __init__(self, data):
-        self.data = data
-
-    def compress(self):
+    def __init__(self):
         pass
 
-    def _split_to_segments(self):
-        pass
-
-    def _model_signal(self):
-        pass
-
-    def _levinson_durbin(self):
-        pass
-
-    def _calculate_residual_errors(self):
-        pass
-
-    def _save_file(self):
+    def reconstruct_signal(self):
         pass
