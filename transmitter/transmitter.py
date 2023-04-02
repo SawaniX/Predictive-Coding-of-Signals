7c27d5aec13c3abbd36da418b8d536fee1ee8148
SawaniX <tomekwalburg@wp.pl>
2023-04-02 22:57:59 +0200

    calculating p

--- transmitter/transmitter.py
+++ transmitter/transmitter.py
@@ -6,18 +6,28 @@ from transmitter.prepare_data import PrepareData
 class Transmitter:
     def __init__(self, data: np.ndarray):
         self.data = data
-        
+
         self.preparator = PrepareData(self.data)
 
     def compress(self):
         segments = self.preparator.prepare()
+        self._levinson_durbin(segments)
         
 
     def _model_signal(self):
         pass
 
-    def _levinson_durbin(self):
-        pass
+    def _levinson_durbin(self, segments: list):
+        p = []
+        for segment in segments:
+            part = []
+            for i, sample in enumerate(segment, 1):
+                pi = 0
+                for t in range(i+1, len(segment)):
+                    pi += segment[t] * segment[t-i]
+                part.append(pi)
+            p.append(part)
+        print(p[-1])
 
     def _calculate_residual_errors(self):
         pass
